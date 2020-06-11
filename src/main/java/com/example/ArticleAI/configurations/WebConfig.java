0ba82ce0d4513e33bc9af424cbbb190324c54dcf
hyptexxx <ilya-2379@yandex.ru>
2020-06-11 23:05:51 +0300

    changed documents folder structure

--- src/main/java/com/example/ArticleAI/configurations/WebConfig.java
+++ src/main/java/com/example/ArticleAI/configurations/WebConfig.java
@@ -1,9 +1,7 @@
 package com.example.ArticleAI.configurations;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
